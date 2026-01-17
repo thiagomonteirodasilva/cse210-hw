439ce6b08ea58ef7573ab5aa169e2afff06a17cb
thiagomonteirodasilva <thiagom.silva195@gmail.com>
2026-01-17 15:46:46 -0300

    w02 explain abstraction

---
+++ week02/Car/Car.cs
@@ -0,0 +1,6 @@
+public class Car
+{
+  public string model;
+  public string brand;
+  public int year;
+}
---
+++ week02/Car/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World Car");
+    Car car = new Car();
+    car.model = "Focus";
+    car.brand = "Ford";
+    car.year = 2019;
+
+    Console.WriteLine($"Brand: {car.brand}, model: {car.model}, year: {car.year}");
+  }
+}
